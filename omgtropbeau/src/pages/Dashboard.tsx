154bd24201bcd7441c6063eccbcad1b537d4c680
Matthieu Fontaine <matth76330@live.fr>
2024-04-27 09:26:34 +0200

    strat login

--- omgtropbeau/src/pages/Dashboard.tsx
+++ omgtropbeau/src/pages/Dashboard.tsx
@@ -9,10 +9,22 @@ const Dashboard: React.FC = () => {
   }, []);
 
   return (
-    <div className='w-full h-screen bg-admin-gray'>
+    <div className='w-full h-screen bg-admin-gray flex flex-col'>
       <Header name='Dashboard' />
       <SideBar />
-      Dashboard
+      <div className="m-3 p-2 rounded-lg bg-white">
+        <form className="flex flex-col">
+          <label className="text-lg font-bold">Email</label>
+          <input type="text" className="border-2 border-gray-300 p-2 rounded-lg" />
+          <label className="text-lg font-bold">Username</label>
+          <input type="text" className="border-2 border-gray-300 p-2 rounded-lg" />
+          <label className="text-lg font-bold">Password</label>
+          <input type="text" className="border-2 border-gray-300 p-2 rounded-lg" />
+          <label className="text-lg font-bold">Confirm Password</label>
+          <input type="text" className="border-2 border-gray-300 p-2 rounded-lg" />
+          <button className="bg-blue-500 text-white p-2 rounded-lg mt-2">Enregistrer</button>
+        </form>
+      </div>
     </div>
   );
 };
